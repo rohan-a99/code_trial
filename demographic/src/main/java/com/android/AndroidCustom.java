0f2828cd978d35924212bd9e15100fa2fbfead87
root <rohankar1999@gmail.com>
2021-04-28 02:15:42 +0800

    trial

---
+++ demographic/src/main/java/com/android/AndroidCustom.java
@@ -0,0 +1,18 @@
+package com.android;
+
+
+
+public class AndroidCustom{
+
+public void custom1(){
+System.out.println("Custom1 method is working fine");
+}
+
+public static void main(String[] args)
+{
+AndroidCustom obj = new AndroidCustom();
+obj.custom1();
+
+}
+}
+
